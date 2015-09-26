44de1746a202429c4e9e5224e03f76ef49006e20
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2015-09-26 16:21:08 +0200

    Monadic, vé

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, MultiParamTypeClasses #-}
 
 -----------------------------------------------------------------------------
 --
@@ -20,11 +20,42 @@ module Main (
 
 import Debug.Trace
 
-import Quipper
-import Quipper.QData
-import Quipper.Monad
+import qualified Quipper as Q
+import qualified Quipper.QData as QD
 
--- AST for Circuits
+class (Monad m) => QMonad m q where
+    hadamard :: q -> m q
+    qnot_at :: q -> m ()
+    controlled :: m a -> [q] -> m a
+    with_ancilla :: (q -> m a) -> m a
+
+class (Monad m) => QControl m where
+    with_controls :: Q.ControlList -> m a -> m a
+
+class QCheck q where
+    (.==.) :: q -> Bool -> Q.ControlList
+    infix 4 .==.
+
+(.&&.) :: Q.ControlList -> Q.ControlList -> Q.ControlList
+(.&&.) = (Q..&&.)
+infixr 3 .&&.
+
+(..==..) :: Q.Qubit -> Bool -> Q.ControlList
+q ..==.. i = (Q..==.) q i
+
+instance QMonad Q.Circ Q.Qubit where
+    hadamard = Q.hadamard
+    qnot_at = Q.qnot_at
+    controlled = Q.controlled
+    with_ancilla = Q.with_ancilla
+
+instance QControl Q.Circ where
+    with_controls = Q.with_controls
+
+instance QCheck Q.Qubit where
+    (.==.) = (..==..)
+
+-- AST for muits
 data Circuit a q where
     Input :: a -> Circuit a q
     Init :: Bool -> Circuit q q
@@ -45,44 +76,44 @@ indent :: (Num a, Eq a) => a -> String
 indent 0 = "|"
 indent i = "|    " ++ indent (i-1)
 
--- AST to Circ
-eval' :: (Show a) => Circuit a Qubit -> Circ a
-eval' (Input x) = return x
-eval' (Init b) = qinit b
-eval' (QNotControlled w c) = do
-    qw <- eval' w
-    qc <- eval' c
-    qnot qw `controlled` qc
-eval' (Hadamard x) = do
-    qx <- eval' x
-    hadamard qx
 
-eval :: (Show a, Show b) => (Circuit a Qubit -> Circuit b Qubit) -> a -> Circ b
-eval f i = eval' $ f (Input i)
+-- This file is part of Q. Copyright (C) 2011-2014. Please see the
+-- file COPYRIGHT for a list of authors, copyright holders, licensing,
+-- and other details. All rights reserved.
+--
+-- ======================================================================
 
--- Example AST
-example1 :: Qubit -> Circ Qubit
-example1 x = do
-    a <- qinit False
-    b <- hadamard a
-    c <- hadamard x
-    d <- qnot c `controlled` b
-    e <- hadamard d
-    f <- qnot e `controlled` b
-    return f
+circuit :: (QMonad m q) => q -> q -> q -> m (q, q, q)
+circuit a b c = do
+  qnot_at a `controlled` [b]
+  qnot_at b `controlled` [c]
+  hadamard c `controlled` [a,b]
+  return (a, b, c)
 
-example2 :: Qubit -> Circ Qubit
-example2 = eval example_ast
+hadamard2 :: (QMonad m q) => q -> q -> q -> m (q, q, q)
+hadamard2 h a b = do
+  with_ancilla (f h a b)
+  return (h, a, b) where
 
-example_ast x = result where
-    a = Init False
-    b = Hadamard a
-    c = Hadamard x
-    d = QNotControlled c b
-    e = Hadamard d
-    f = QNotControlled e d
-    result = f
+f :: (QMonad m q) => q -> q -> q -> q -> m ()
+f h a b c = do
+  qnot_at c `controlled` [a, b]
+  hadamard h `controlled` [c]
+  qnot_at c `controlled` [a, b]
 
-main = do
-    print_simple Preview example1
-    print_simple Preview example2
+example :: (QMonad m q, QControl m, QCheck q) => q -> q -> q -> q -> q -> m (q, q, q, q, q)
+example a b c d e = do
+  circuit a b c
+  circuit b c a
+  with_controls (d .==. 1 .&&. e .==. 0) $ do
+      circuit a b c
+      circuit b c a
+  circuit a b c
+  circuit b c a
+  return (a, b, c, d, e)
+
+example' :: Q.Qubit -> Q.Qubit -> Q.Qubit -> Q.Qubit -> Q.Qubit ->
+    Q.Circ (Q.Qubit, Q.Qubit, Q.Qubit, Q.Qubit, Q.Qubit)
+example' = example
+
+main = Q.print_simple Q.Preview example'
