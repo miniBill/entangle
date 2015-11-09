6a1550daf5de29e8bdf93f2990e78bea9bb67916
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2015-11-09 20:05:24 +0100

    Simbolic constructors for Expr

--- src/SqMath.hs
+++ src/SqMath.hs
@@ -1,21 +1,25 @@
 {-# LANGUAGE PatternGuards #-}
 
-module SqMath where
+module SqMath (Expr) where
 
 import Data.Ratio
 
 data Expr = Leaf Integer
-          | Sum Expr Expr
-          | Mult Expr Expr
-          | Div Expr Expr
+          | Expr :+: Expr
+          | Expr :*: Expr
+          | Expr :/: Expr
           | Abs Expr
           | Sqrt Expr
 
+infixl 6 :+:
+infixl 7 :*:
+infixl 7 :/:
+
 eval :: Expr -> Either Integer Double
 eval (Leaf i) = Left i
-eval (Sum i j) = lift2 (+) (+) i j
-eval (Mult i j) = lift2 (*) (*) i j
-eval (Div i j) = lift2 (div) (/) i j
+eval (i :+: j) = lift2 (+) (+) i j
+eval (i :*: j) = lift2 (*) (*) i j
+eval (i :/: j) = lift2 (div) (/) i j
 eval (Abs i) = lift1 (abs) (abs) i
 eval (Sqrt i) = (Right . sqrt . to_double . eval) i
 
@@ -34,8 +38,8 @@ lift2 i d e f = lift' (eval e) (eval f) where
     lift' x y = Right $ d (to_double x) (to_double y)
 
 instance Num Expr where
-    a + b = simplify $ Sum a b
-    a * b = simplify $ Mult a b
+    a + b = simplify $ a :+: b
+    a * b = simplify $ a :*: b
     abs a = simplify $ Abs a
     signum = undefined
     fromInteger = Leaf
@@ -43,7 +47,7 @@ instance Num Expr where
 
 instance Fractional Expr where
     fromRational r = Leaf (numerator r) / Leaf (numerator r)
-    a / b = simplify $ Div a b
+    a / b = simplify $ a :/: b
 
 instance Floating Expr where
     sqrt a = simplify $ Sqrt a
@@ -61,30 +65,30 @@ instance Floating Expr where
     acosh = undefined
     atanh = undefined
 
-simplify (Sum (Leaf a) (Leaf b)) = Leaf $ a + b
-simplify (Sum (Leaf 0) a) = simplify a
-simplify (Sum a (Leaf 0)) = simplify a
-simplify (Sum (Div a b) c) = (a + b * c) / b
-simplify (Sum a (Div b c)) = (a * c + b) / c
+simplify ((Leaf a) :+: (Leaf b)) = Leaf $ a + b
+simplify ((Leaf 0) :+: a) = simplify a
+simplify (a :+: (Leaf 0)) = simplify a
+simplify ((a :/: b) :+: c) = (a + b * c) / b
+simplify (a :+: (b :/: c)) = (a * c + b) / c
 
-simplify (Mult (Leaf 0) _) = 0
-simplify (Mult _ (Leaf 0)) = 0
-simplify (Mult (Leaf 1) a) = simplify a
-simplify (Mult a (Leaf 1)) = simplify a
-simplify (Mult (Leaf a) (Leaf b)) = Leaf $ a * b
-simplify (Mult (Div a b) c) = (a * c) / b
-simplify (Mult a (Div b c)) = (a * b) / c
-simplify (Mult (Sqrt a) (Sqrt b)) = sqrt $ a * b
-simplify (Mult (Mult a b) c) = a * (b * c)
-simplify (Mult a (Leaf b)) = Leaf b * a
-simplify (Mult (Leaf a) (Mult (Leaf b) c)) = Leaf (a * b) * c
-simplify (Mult a (Mult b@(Leaf _) c)) = b * (a * c)
+simplify ((Leaf 0) :*: _) = 0
+simplify (_ :*: (Leaf 0)) = 0
+simplify ((Leaf 1) :*: a) = simplify a
+simplify (a :*: (Leaf 1)) = simplify a
+simplify ((Leaf a) :*: (Leaf b)) = Leaf $ a * b
+simplify ((a :/: b) :*: c) = (a * c) / b
+simplify (a :*: (b :/: c)) = (a * b) / c
+simplify ((Sqrt a) :*: (Sqrt b)) = sqrt $ a * b
+simplify ((a :*: b) :*: c) = a * (b * c)
+simplify (a :*: (Leaf b)) = Leaf b * a
+simplify ((Leaf a) :*: ((Leaf b) :*: c)) = Leaf (a * b) * c
+simplify (a :*: (b@(Leaf _) :*: c)) = b * (a * c)
 
-simplify (Div a s@(Sqrt _)) = (a * s) / (s * s)
-simplify (Div (Div a b) c) = a / (b * c)
-simplify (Div a (Div b c)) = (a * c) / b
-simplify (Div (Leaf a) (Leaf b)) | a `mod` b == 0 = Leaf $ a `div` b
-simplify (Div (Mult (Leaf a) b) (Leaf c)) | a `mod` c == 0 = (Leaf $ a `div` c) * b
+simplify (a :/: s@(Sqrt _)) = (a * s) / (s * s)
+simplify ((a :/: b) :/: c) = a / (b * c)
+simplify (a :/: (b :/: c)) = (a * c) / b
+simplify ((Leaf a) :/: (Leaf b)) | a `mod` b == 0 = Leaf $ a `div` b
+simplify (((Leaf a) :*: b) :/: (Leaf c)) | a `mod` c == 0 = (Leaf $ a `div` c) * b
 
 simplify (Sqrt (Leaf a)) | Just r <- perfectSqrt a = Leaf r
 
@@ -101,8 +105,8 @@ perfectSqrt _ = Nothing
 instance Show Expr where
     showsPrec p e0 = case e0 of
         (Leaf i) -> shows i
-        (Sum x y) -> showParen (p >= 6) $ (showsPrec 6 x) . (" + " ++) . (showsPrec 6 y)
-        (Mult x y) -> showParen (p >= 7) $ (showsPrec 7 x) . (" * " ++) . (showsPrec 7 y)
-        (Div x y) -> showParen (p >= 7) $ (showsPrec 7 x) . (" / " ++) . (showsPrec 7 y)
+        (x :+: y) -> showParen (p >= 6) $ (showsPrec 6 x) . (" + " ++) . (showsPrec 6 y)
+        (x :*: y) -> showParen (p >= 7) $ (showsPrec 7 x) . (" * " ++) . (showsPrec 7 y)
+        (x :/: y) -> showParen (p >= 7) $ (showsPrec 7 x) . (" / " ++) . (showsPrec 7 y)
         (Abs a) -> ("abs(" ++) . showsPrec 10 a . (++ ")")
         (Sqrt a) -> ("sqrt(" ++) . showsPrec 10 a . (++ ")")
