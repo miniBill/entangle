7a33cccca925cccdc0b2454edc0989219d260b12
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2017-03-16 21:47:07 +0100

    Skeleton

--- src/exe/Main.elm
+++ src/exe/Main.elm
@@ -1,1 +1,43 @@
 module Main exposing (main)
+
+import Html exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    program
+        { view = view
+        , update = update
+        , subscriptions = subscriptions
+        , init = init
+        }
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = NoOp
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    text <| toString model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
